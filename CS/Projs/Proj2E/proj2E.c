31dfbb919757a5a0c39ad86bab03051a80531006
ethanhyde <ehyde9@gmail.com>
2022-10-25 21:10:08 -0700

    CS

--- CS/Projs/Proj2E/proj2E.c
+++ CS/Projs/Proj2E/proj2E.c
@@ -2,34 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-int SearchForWord(char word)
+int main(int argc, char *argv [])
 {
-	int counter = 0; //Value to be returned
-
+	int counter = 0;
 	//Opens file and ensures it exists
-	FILE *h = fopen("file.txt", "r");
+	FILE *h = fopen(argv[1], "r");
 	if(!fopen)
 		{
 			printf("File does not exist\n");
+			return 1;
 		}
 	
-	
+	//Searches file and copies contents 
+	fseek(h, 0, SEEK_END);
+	long max = ftell(h);
+	char *contents = (char *) malloc(sizeof(char) * max);
+	rewind(h);
+	fread(contents, sizeof (char), max, h);
+	fclose(h);
 
-	return count; 
-}	
+	for (int i = 0; i < max; ++i)
+	{
+		printf("%c", contents[i]);
+	}
 
-int main(int argc, char argv [])
-{
-	int count;
-	
-	for(int i = 0; i < argc; ++i)
+	int *count = (int *) malloc(sizeof (int) * argc);
+
+	int result;
+	for(int i = 2; i < argc; ++i)
 		{
-			//Sends each argc string into the function
-			//Returns the count of how many times it occurred in file
-			count = SearchForWord(argv[i]);
+			
 		}
-	
 
 
-return 0;
+	free(contents);
+	return 0;
 }
